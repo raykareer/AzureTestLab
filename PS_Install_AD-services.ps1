836d4445a3f27c166c4a4fa1f40cb1a0090ae38a
raykareer <44954961+raykareer@users.noreply.github.com>
2020-12-09 14:50:16 -0800

    Update PS_Install_AD-services.ps1

--- PS_Install_AD-services.ps1
+++ PS_Install_AD-services.ps1
@@ -1,5 +1,6 @@
-Add-Type -AssemblyName 'System.Web'
-$passwd = [System.Web.Security.Membership]::GeneratePassword(20,4) -replace '[^a-z 0-9]',''
+#Add-Type -AssemblyName 'System.Web'
+#$passwd = [System.Web.Security.Membership]::GeneratePassword(20,4) -replace '[^a-z 0-9]',''
+$passwd = "P@ssw0rd"
 
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 
