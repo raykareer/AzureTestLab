51c3ada0a2999af05cb491478c797568018454f1
raykareer <44954961+raykareer@users.noreply.github.com>
2020-12-09 00:27:14 -0800

    Update PS_Install_AD-services.ps1

--- PS_Install_AD-services.ps1
+++ PS_Install_AD-services.ps1
@@ -1,4 +1,6 @@
-Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 Add-Type -AssemblyName 'System.Web'
 $passwd = [System.Web.Security.Membership]::GeneratePassword(20,4) -replace '[^a-z 0-9]',''
-Install-ADDSDomainController -DomainName mydomain.org -SafeModeAdministratorPassword (ConvertTo-SecureString $passwd -AsPlainText -Force)
+
+Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest -DomainName "sfhr.test" -Force -SafeModeAdministratorPassword (ConvertTo-SecureString $passwd -AsPlainText -Force)
