28894a5d016ceaa9c98c1470b21c4a683429cbe8
raykareer <44954961+raykareer@users.noreply.github.com>
2020-12-09 20:33:59 -0800

    Create setupADusers.ps1

---
+++ setupADusers.ps1
@@ -0,0 +1,5 @@
+
+New-ADUser -GivenName "Bruce Willis" -Name bwillis
+New-ADUser -GivenName "Halle Berry" -Name hberry
+New-ADUser -GivenName "Gene Hackman" -Name ghackman
+New-ADUser -GivenName "David Letterman" -Name dletterman
