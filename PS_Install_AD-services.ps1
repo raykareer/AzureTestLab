275d639e437a1288d1c3fdb25a3311851679a4fc
raykareer <44954961+raykareer@users.noreply.github.com>
2020-12-09 23:55:19 -0800

    Update PS_Install_AD-services.ps1

--- PS_Install_AD-services.ps1
+++ PS_Install_AD-services.ps1
@@ -2,6 +2,11 @@ Add-Type -AssemblyName 'System.Web'
 $passwd = [System.Web.Security.Membership]::GeneratePassword(20,4) -replace '[^a-z 0-9]',''
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "sfhr.test" -Force -SafeModeAdministratorPassword (ConvertTo-SecureString $passwd -AsPlainText -Force)
-$setupADusers = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/raykareer/AzureTestLab/main/setupADusers.ps1"
+$startupScript = {$setupADusers = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/raykareer/AzureTestLab/main/setupADusers.ps1"
 Invoke-Expression $($setupADusers.content)
+Remove-Item C:\Windows\System32\GroupPolicy\Machine\Scripts\Startup\startup.ps1 -Force
+}
+
+$startupScript | out-file C:\Windows\System32\GroupPolicy\Machine\Scripts\Startup\startup.ps1
+
 Restart-Computer
