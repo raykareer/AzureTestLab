09f4761850c2fcbf0dcc98b8ea5a0c1406cb8c41
raykareer <44954961+raykareer@users.noreply.github.com>
2021-04-17 20:03:13 -0700

    Update PS_Install_AD-services.ps1

--- PS_Install_AD-services.ps1
+++ PS_Install_AD-services.ps1
@@ -3,6 +3,5 @@ $passwd = [System.Web.Security.Membership]::GeneratePassword(20,4) -replace '[^a
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "sfhr.test" -Force -SafeModeAdministratorPassword (ConvertTo-SecureString $passwd -AsPlainText -Force)
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/raykareer/AzureTestLab/main/setupADusers.ps1"
-Invoke-Expression $($setupADusers.content)
 
 Restart-Computer
